873239757778fa31ab88a8ecd45aeb373fe87d92
spb71 <shem.baijoo@gmail.com>
2021-12-10 22:46:21 -0500

    Implemented workout model

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for the exercise",
+      },
+      duration: {
+        type: Number,
+        required: "Enter an exercise duration (in minutes)",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
---
+++ routes/api.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const db = require("../models");
+const path = require("path");
+
+module.exports = router;
