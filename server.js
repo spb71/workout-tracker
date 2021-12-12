f9282a95b1aacd94ef573c5905237a566a245160
spb71 <shem.baijoo@gmail.com>
2021-12-11 21:34:13 -0500

    Fixed typo in server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
