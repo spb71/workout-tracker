f22e692a32ab0c3c17f6b8d25473a80a5af36cb4
spb71 <shem.baijoo@gmail.com>
2021-12-11 21:25:44 -0500

    Implemented get, post, and put routes for workouts page

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,65 @@
 const router = require("express").Router();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const db = require("../models");
-const path = require("path");
+// const path = require("path");
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      console.log(dbWorkouts);
+      const workouts = dbWorkouts.map((workout) => {
+        console.log({ workout });
+        const duration = workout.exercises.reduce((acc, next) => {
+          return acc + next.duration;
+        }, 0);
+
+        return {
+          totalDuration: duration,
+          ...workout.toObject(),
+        };
+      });
+
+      res.json(workouts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  console.log(req.body);
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
